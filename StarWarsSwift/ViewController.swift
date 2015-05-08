ee0568fc5afa5ef534981bd526ba94ab0b600305
Carmelo Ruymán Quintana Santana <canariseven@gmail.com>
2015-05-08 22:06:33 +0100

    Initial Commit

---
+++ StarWarsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWarsSwift
+//
+//  Created by Carmelo Ruymán Quintana Santana on 8/5/15.
+//  Copyright (c) 2015 Carmelo Ruymán Quintana Santana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
